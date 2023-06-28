db0184702850ec2aa15f42b7269a6ce273c3b06e
lubkepatryk <lubke.patryk@gmail.com>
2023-06-28 18:52:16 +0200

    code refactored to use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Feedback from './Feedback/Feedback';
 
 export const App = () => {
@@ -16,3 +17,5 @@ export const App = () => {
     </div>
   );
 };
+
+export default App;
